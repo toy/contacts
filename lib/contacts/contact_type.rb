b7414264b4266741ab2c1122cf12ff25564a32fd
Boba Fat <ivan@workisfun.ru>
2010-01-14 20:50:30 +0300

    `has_contacts`/`has_contact` instead of `include Contacts`

--- lib/contacts/contact_type.rb
+++ lib/contacts/contact_type.rb
@@ -46,13 +46,81 @@ module Contacts
     end
   end
 
+  module ClassMethods
+    def has_contacts(*args)
+      options = args.extract_options!
+      args = args + Contacts.contact_types.keys if args.delete(:all)
+      args.map(&:to_sym).uniq.each do |name|
+        name = name.to_sym
+        as = (options[:as] || name).to_sym
+        class_eval %Q{
+          def #{name}=(value)
+            set_contact(#{name.inspect}, value, #{{:as => as}.inspect})
+          end
+          def #{name}
+            get_contact(#{name.inspect}, #{{:as => as}.inspect})
+          end
+          def #{name}_link
+            format_contact(#{name.inspect}, #{{:as => as}.inspect})
+          end
+          def #{name}?
+            #{name}.present?
+          end
+        }, __FILE__, __LINE__
+      end
+      unless include?(InstanceMethods)
+        serialize :contacts, Hash
+        validate :contacts_must_be_valid
+        include InstanceMethods
+      end
+    end
+    alias_method :has_contact, :has_contacts
+  end
+
+  module InstanceMethods
+  private
+    def set_contact(name, value, options = {})
+      contacts_will_change!
+      self.contacts ||= {}
+      if contact_type = Contacts.contact_types[options[:as]]
+        contacts[name] = contact_type.sanitize(value) || value
+      else
+        raise "Unknown contact type: #{options[:as].inspect}"
+      end
+    end
+
+    def get_contact(name, options = {})
+      contacts && contacts[name]
+    end
+
+    def format_contact(name, options = {})
+      value = get_contact(name, options)
+      if contact_type = Contacts.contact_types[options[:as]]
+        contact_type.format(value) if value.present?
+      else
+        raise "Unknown contact type: #{options[:as].inspect}"
+      end
+    end
+
+    def contacts_must_be_valid
+      if contacts
+        contacts.each do |name, value|
+          if value.present?
+            if contact_type = Contacts.contact_types[name]
+              errors.add(name) unless contact_type.sanitize(value).present?
+            end
+          end
+        end
+      end
+    end
+  end
+
   def self.contact_types
     @contact_types ||= {}
   end
 
   def self.included(base)
-    base.send :serialize, :contacts, Hash
-    base.send :validate, :contacts_must_be_valid
+    base.extend ClassMethods
   end
 
   def self.sorted
@@ -74,56 +142,5 @@ module Contacts
       RAILS_DEFAULT_LOGGER.warn "contact type #{name} already defined"
     end
     contact_types[name] = contact_type
-    class_eval %Q{
-      def #{name}=(value)
-        set_contact(#{name.inspect}, value)
-      end
-      def #{name}
-        get_contact(#{name.inspect})
-      end
-      def #{name}?
-        #{name}.present?
-      end
-      def #{name}_link
-        format_contact(#{name.inspect})
-      end
-    }, __FILE__, __LINE__
-  end
-
-private
-
-  def set_contact(name, value)
-    contacts_will_change!
-    self.contacts ||= {}
-    if contact_type = Contacts.contact_types[name]
-      contacts[name] = contact_type.sanitize(value) || value
-    else
-      raise "Unknown contact type: #{name.inspect}"
-    end
-  end
-
-  def get_contact(name)
-    contacts && contacts[name]
-  end
-
-  def format_contact(name)
-    value = get_contact(name)
-    if contact_type = Contacts.contact_types[name]
-      contact_type.format(value) if value.present?
-    else
-      raise "Unknown contact type: #{name.inspect}"
-    end
-  end
-
-  def contacts_must_be_valid
-    if contacts
-      contacts.each do |name, value|
-        if value.present?
-          if contact_type = Contacts.contact_types[name]
-            errors.add(name) unless contact_type.sanitize(value).present?
-          end
-        end
-      end
-    end
   end
 end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -1,10 +1,35 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
+class Post < ActiveRecord::Base
+end
+
 class User < ActiveRecord::Base
-  include Contacts
+  has_contacts :icq, :skype, :test
+  has_contacts :test_a, :test_b, :as => :test
+  has_contact :test_c, :as => :test
+  has_contact :test_z
+end
+
+class User2 < User
+  has_contacts :all
 end
 
 describe Contacts do
+  it "should not auto insert accessors" do
+    Post.new.should_not respond_to(:icq, :icq=, :icq?, :icq_link)
+  end
+
+  it "should auto insert all accessors which were asked for" do
+    User.new.should respond_to(:icq, :icq=, :icq?, :icq_link)
+    User.new.should_not respond_to(:facebook, :facebook=, :facebook?, :facebook_link)
+  end
+
+  it "should auto insert all accessors if asked for all" do
+    Contacts.contact_types.keys.each do |contact|
+      User2.new.should respond_to(:"#{contact}", :"#{contact}=", :"#{contact}?", :"#{contact}_link")
+    end
+  end
+
   it "should store test string" do
     Contacts.contact :test
     User.create(:test => '12345')
@@ -124,32 +149,78 @@ describe Contacts do
     end
   end
 
+  describe "cloning contacs" do
+    it "should get rules for contact with :as => :test from test contact" do
+      Contacts.contact :test do
+        sanitizer %r{[b-e]+}
+        formatter 'hello %s'
+      end
+      Contacts.contact :test_a
+      Contacts.contact :test_b
+      Contacts.contact :test_c
+      Contacts.contact :test_z
+
+      user = User.create do |u|
+        u.test = 'abcdef'
+        u.test_a = 'abcdef'
+        u.test_b = 'abcdef'
+        u.test_c = 'abcdef'
+        u.test_z = 'abcdef'
+      end
+
+      user.test.should == 'bcde'
+      user.test_a.should == 'bcde'
+      user.test_b.should == 'bcde'
+      user.test_c.should == 'bcde'
+      user.test_z.should == 'abcdef'
+      user.test_link.should == 'hello bcde'
+      user.test_a_link.should == 'hello bcde'
+      user.test_b_link.should == 'hello bcde'
+      user.test_c_link.should == 'hello bcde'
+      user.test_z_link.should == 'abcdef'
+    end
+  end
+
   describe "sorting and duplicates" do
     it "should complain about sort order if it is turned on" do
       RAILS_DEFAULT_LOGGER.should_receive(:warn).with('contact type b is out of order')
-      Contacts.contact_types.replace({})
-      Contacts.sorted do
-        Contacts.contact :a
-        Contacts.contact :c
-        Contacts.contact :b
+      contact_types = Contacts.contact_types.dup
+      begin
+        Contacts.contact_types.replace({})
+        Contacts.sorted do
+          Contacts.contact :a
+          Contacts.contact :c
+          Contacts.contact :b
+        end
+      ensure
+        Contacts.contact_types.replace(contact_types)
       end
     end
 
     it "should not complain about sort order if it is not turned on" do
       RAILS_DEFAULT_LOGGER.should_not_receive(:warn)
-      Contacts.contact_types.replace({})
-      Contacts.contact :a
-      Contacts.contact :c
-      Contacts.contact :b
+      contact_types = Contacts.contact_types.dup
+      begin
+        Contacts.contact_types.replace({})
+        Contacts.contact :a
+        Contacts.contact :c
+        Contacts.contact :b
+      ensure
+        Contacts.contact_types.replace(contact_types)
+      end
     end
 
     it "should complain about duplicates" do
       RAILS_DEFAULT_LOGGER.should_receive(:warn).with('contact type a already defined')
-      Contacts.contact_types.replace({})
-      Contacts.contact :a
-      Contacts.contact :b
-      Contacts.contact :a
+      contact_types = Contacts.contact_types.dup
+      begin
+        Contacts.contact_types.replace({})
+        Contacts.contact :a
+        Contacts.contact :b
+        Contacts.contact :a
+      ensure
+        Contacts.contact_types.replace(contact_types)
+      end
     end
-
   end
 end
